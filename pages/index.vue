48730e727283be02bb98cc0121df1790129fa537
Arwen <380616679@qq.com>
2019-03-14 13:52:13 +0800

    [新增]第一次添加

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="index">
+    <h1>开始！</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+h1 {
+  text-align: center;
+}
+</style>
